a19e2fe6a41502dba7f9be5e3e65e42b73218bcf
Sam Porter Giffords <sampgiffords@Sams-MBP.localdomain>
2018-05-09 13:15:55 -0700

    Blog post display now works

--- ria-sam/scripts/article.js
+++ ria-sam/scripts/article.js
@@ -15,28 +15,41 @@ function Article (rawDataObj) {
   this.body = rawDataObj.body;
 
   // Save ALL the properties of `rawDataObj` into `this`
-  
-
-
 }
+
+// console.log(articles);
 
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
   // PUT YOUR RESPONSE HERE
-  //There are two main benefits
+  //There are three main benefits
   //a)  Manual entry is not needed, which saves time.
   // b)  Manual entry also has the potential to introduce errors.  Cloning mitigates this risk.
+  // c) Using the cloning method eliminates cluttered code.  
 
   let $newArticle = $('article.template').clone();
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  $newArticle.removeClass('template');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
-  $newArticle.attr('data-author', this.author);
-  $newArticle.attr('data-authorUrl', this.authorUrl);
-  $newArticle.attr('data-title', this.title);
-  $newArticle.attr('data-body', this.body);
+  // $newArticle.attr('div a', this.author);
+  // $newArticle.attr('div a', this.authorUrl);
+  $newArticle.attr('h1', this.title);
+  // $newArticle.attr('.article-body', this.body);
   $newArticle.attr('data-publishedOn', this.publishedOn);
+  
+  // $(newArticle).find('.draft').append(newArticle.this);
+  // $newArticle.find('.draft').append(newArticle.this);
+  // console.log($newArticle);
+
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('time').html(this.publishedOn);
+  $newArticle.find('.article-body').html(this.body);
+
+
   
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
@@ -59,11 +72,40 @@ rawData.sort(function(a,b) {
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-  console.log('raw data', rawData[i]);
-}
+// for(let i = 0; i < rawData.length; i++) {
+//   articles.push(new Article(rawData[i]));
+//  console.log('raw data', rawData[i]);
+// }
 
-for(let i = 0; i < articles.length; i++) {
-  $('#articles').append(articles[i].toHtml());
-}
+// for(let i = 0; i < 5; i++) {
+//   articles.push(new Article(rawData[i]));
+//  console.log('raw data', rawData[i]);
+// }
+
+rawData.forEach(function(printThis) {
+  articles.push(new Article(printThis));
+  console.log('printThis + ', printThis);
+});
+
+
+
+// rawData.forEach(Article(rawDataObj) {
+//   articles.push(new Article(rawData[i]));
+//   console.log('raw data', rawData[i]);
+// });
+
+// for(let i = 0; i < articles.length; i++) {
+//   $('#articles').append(articles[i].toHtml());
+// }
+
+// for(let i = 0; i < 5; i++) {
+//   $('#articles').append(articles[i].toHtml());
+// }
+
+articles.forEach(function(printArt) {
+  $('#articles').append(printArt.toHtml());
+  });
+
+// forEach(newArticle{
+//   $('#articles').append(articles[i].toHtml());
+// }
