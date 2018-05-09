779c4a805badb5854aad26874dc1c217a915fe3f
MARIO NISHIO <mario.nishio@gmail.com>
2018-05-09 10:09:29 -0700

    rendered the instances on page, removed template, cloned template to render

--- mario-ashton/scripts/article.js
+++ mario-ashton/scripts/article.js
@@ -18,13 +18,20 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // Cloning the template will allow the continual usage of the tempalte to be filled with our Article instances. It is important because the HTML structure is kept consistent. 
+  // Cloning the template will allow the continual usage of the tempalte to be filled with our Article instances. It is important because the HTML structure is kept consistent 
 
   let $newArticle = $('article.template').clone();
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  $newArticle.removeClass('template');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
+
+  $newArticle.find('address > a').text(this.author);
+  $newArticle.find('address > a').prop('href', this.authorUrl);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('.article-body').text(this.body);
+  $newArticle.find('time').prop('datetime', this.publishedOn);
 
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
