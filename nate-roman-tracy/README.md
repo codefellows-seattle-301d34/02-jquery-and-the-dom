fc104c41caafdc284b87ff27aa55553018141fa3
NateA275 <nateaurich@gmail.com>
2018-05-09 12:14:17 -0700

    Completed COMMENT and REVIEW items. Added README content.

--- nate-roman-tracy/README.md
+++ nate-roman-tracy/README.md
@@ -2,30 +2,36 @@
 **Version**: 1.0.1
 
 ## Overview
-This application is a timed practice to enhance our understanding of the Media Query, SMACCS, and CSS on fixed HTML. 
+This application is an exercise in DOM manipulation via jQuery, familiarizaion with the Array methods .forEach() and .sort(), and object constructors.
 <!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for a Code Fellows 301 class. (i.e. What's your problem domain?) -->
 
 ## Getting Started
-To get strated, clone the original repo from CF GitHub.
+To get strated, clone the repo from CF GitHub.
+https://github.com/TCW417/02-jquery-and-the-dom.git
+Checkout branch 'nate-roman-tracy',
+Open index.html.
 <!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
 
 ## Architecture
 HTML and CSS were used to create this app. SMACSS.
+jQuery - https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+Icons courtesy https://icomoon.io
 <!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
 
-## Change Log
-05-08-2018 12:30pm - Application was completed to look like the provided template. 
+## Change Log 
+05-09-2018 12:30pm - Created Scalable content generation from data file
 <!-- Use this are to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
 
 01-01-2001 4:59pm - Application now has a fully-functional express server, with GET and POST routes for the book resource.-->
 
 ## Credits and Collaborations
-Thank you Code Fellows for providing the source code to style with out CSS.
-MDN and Stack Overflow for solving questions we had.
+jQuery library
+IcoMoon Icon Fonts
+See Architecture above for links.
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
-Message Input
 
 
-Message Tracy Williams, Nate Aurich
+
+Message Tracy Williams, Nate Aurich, Roman Kireev
 
 *bold* _italics_ ~strike~ `code` ```preformatted``` >quote
--- nate-roman-tracy/scripts/article.js
+++ nate-roman-tracy/scripts/article.js
@@ -2,11 +2,11 @@
 
 let articles = [];
 
-// COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// DONE: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
+// This function is known as a constructor. It's purpose is to generate new instances of objects. It is convention to capiotolize Constructors.
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object that is passed in to complete this constructor function:
+  // DONE: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
@@ -17,18 +17,18 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+  // DONE: What is the benefit of cloning the article? (see the jQuery docs)
+  // This allows us to use the article as a template from which we can clone and repopulate, rather than generate new elements every time.
 
   let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  /* DONE: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
   $newArticle.addClass('clearfix');
   $newArticle.removeClass('template');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  /* DONE: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
       1. author name,
       2. author url,
@@ -41,7 +41,7 @@ Article.prototype.toHtml = function() {
   $newArticle.find('h1').text(this.title);
   $newArticle.find('section.article-body').html(this.body);
   $newArticle.find('time').attr('datetime' , this.publishedOn);
-  // REVIEW: Display the date as a relative number of 'days ago'
+  // DONE: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html(`about ${parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000)} days ago`);
   $newArticle.append('<hr>');
   return $newArticle;
