d99c52091c6fe760d2081464c5bb4dce0b07bf07
Kevin Hwangpo <hwangpo3@gmail.com>
2018-05-09 13:29:44 -0700

    Switched drivers, finished for loop with .foreach, attempted but still TODO on line 25

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -22,7 +22,7 @@ Article.prototype.toHtml = function () {
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
-  // DONE: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  // TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
   //   We need to fill in:
   $newArticle.text(this.author);
   $newArticle.text(this.authorUrl);
@@ -30,30 +30,35 @@ Article.prototype.toHtml = function () {
   $newArticle.text(this.body);
   $newArticle.text(this.publishedOn);
   // REVIEW: Display the date as a relative number of 'days ago'
-  // $newArticle.find('time').html('about '  parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000)  ' days ago');
+  $newArticle.find('time').html(`publish time  ${parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000)}   days ago`);
+  $newArticle.find('address').html(`Author Name ${parseInt((new Date() - new Date(this.author)) / 60 / 60 / 24 / 1000)}   days ago`);
+
+  $newArticle.find('body').html(` ${parseInt((new Date() - new Date(this.body)) / 60 / 60 / 24 / 1000)}   days ago`);
+  
+  $newArticle.find('h1').html(`Title ${parseInt((new Date() - new Date(this.title)) / 60 / 60 / 24 / 1000)}   days ago`);
+
+  $newArticle.find('a').html(`"" ${parseInt((new Date() - new Date(this.authorUrl)) / 60 / 60 / 24 / 1000)}   days ago`);
+
+
   $newArticle.append('<hr>');
   return $newArticle;
 };
+
+
 rawData.sort(function (a, b) {
   // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
-// TODO: Refactor these for loops using the .forEach() array method.
-// for (let i = 0; i < rawData.length; i++) {
-//   articles.push(new Article(rawData[i]));
-// }
+// DONE: Refactor these for loops using the .forEach() array method.
 
-$.forEach(rawData, function (articles) {
-  console.log(articles)
-
-
+// $.forEach(rawData, function (articles) {
+//   console.log(articles)
+// });
+rawData.forEach(function (notes) {
+  articles.push(new Article(notes));
 });
 
+articles.forEach(function (objectList) {
+  $('#articles').append(objectList.toHtml());
+});
 
-
-
-
-
-for (let i = 0; i < articles.length; i++) {
-  $('#articles').append(articles[i].toHtml());
-}
